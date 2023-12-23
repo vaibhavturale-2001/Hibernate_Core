b56663fffcca255bfa3501430ce76ec9eed407a3
Vaibhav Turale <124768342+vaibhavturale-2001@users.noreply.github.com>
2023-12-23 14:21:13 +0530

    save changes

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,59 @@
-package org.example;public class Student {
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="mystudents")
+public class Student{
+
+    @Id
+    private int id;
+    private String name;
+    private String city;
+
+    public Student(int id, String name, String city){
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+// Default constructor
+    public Student(){
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString(){
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
 }
